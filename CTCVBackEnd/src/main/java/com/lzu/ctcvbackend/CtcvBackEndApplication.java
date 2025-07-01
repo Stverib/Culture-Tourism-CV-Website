14dfa9158b8a6f27b10d288cfd105a49012c86d6
Stverib <1716203225@qq.com>
2025-07-01 22:24:24 +0800

    后端初始提交

---
+++ CTCVBackEnd/src/main/java/com/lzu/ctcvbackend/CtcvBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.lzu.ctcvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtcvBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtcvBackEndApplication.class, args);
+	}
+
+}
---
+++ CTCVBackEnd/src/test/java/com/lzu/ctcvbackend/CtcvBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzu.ctcvbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtcvBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
